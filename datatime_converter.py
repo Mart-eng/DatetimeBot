54fe1e948e98735e34b99f0976541087d39550a5
213412341345234523452352345234342@users.noreply.github.com <74334103+Mart-eng@users.noreply.github.com>
2021-11-05 17:41:42 -0400

    starting out

---
+++ datatime_converter.py
@@ -0,0 +1,15 @@
+# datetime_converter.py
+# This program is meant to post multiple timezones with the input of one
+
+import datetime, time
+
+time.strftime("%a, %d %b %Y %H:%M:%S +0000",time.gmtime() )
+def timeca():
+    print("Time in EST:")
+
+    est = time.gmtime()
+    print(est)
+    
+
+
+timeca()
